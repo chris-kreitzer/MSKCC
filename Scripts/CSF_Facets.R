6d99a84e974e274d0d61d2445c8f2bf1e27b4758
chris kreitzer <christoph.kreitzer93@gmx.at>
2022-05-06 13:21:21 +0200

    updated CSF

--- Scripts/CSF_Facets.R
+++ Scripts/CSF_Facets.R
@@ -13,17 +13,66 @@ setup(working.path = '~/Documents/MSKCC/Subhi/CSF/')
 library(facets)
 library(patchwork)
 
-x = facets::readSnpMatrix(filename = '06287_BJ/FROZENPOOLEDNORMAL_IMPACT505_V2.rg.md.abra.printreads__s_C_001327_R018_d.rg.md.abra.printreads.dat.gz')
-x1 = facets::preProcSample(x, gbuild = 'hg19')
-x2 = facets::procSample(x1, cval = 100)
-x3 = facets::emcncf(x2)
+
+IMPACT505 = vroom::vroom('data_gene_panel_impact505.txt', delim = '\t')
+IMPACT505 = as.character(colnames(IMPACT505))
+IMPACT468 = vroom::vroom('~/Documents/GitHub/PARP-Prostate-Cancer/Data/data_gene_panel_impact468.txt', delim = '\t')
+IMPACT468 = as.character(colnames(IMPACT468))
+genes_IMPACT = unique(union(IMPACT468, IMPACT505))
 
 
-y = facetsSuite::run_facets(x, cval = 100, genome = 'hg19', snp_nbhd = 300)
-a = facetsSuite::cnlr_plot(y)
-b = facetsSuite::valor_plot(y)
-c = facetsSuite::icn_plot(y)
 
-a/b/c
+## Folder-wise:
+folders = list.files(path = getwd(), pattern = '^0.*', full.names = T, recursive = F)
 
-facets_fit_qc(y)
+all_out = data.frame()
+for(folder in unique(folders)){
+  try({
+    dir.create(path = paste0(folder, '/FacetsFits'), mode = 777)
+    countFiles = list.files(path = folder, full.names = T)
+    countFiles = countFiles[-grep('FacetsFits', countFiles)]
+    
+    sample_summary = data.frame()
+    for(files in unique(countFiles)){
+      print(basename(files))
+      countmatrix = facetsSuite::read_snp_matrix(input_file = files)
+      countmatrix = countmatrix[, c(1,2,3,5,4,6)]
+      
+      #' normal Facets
+      fit = facetsSuite::run_facets(read_counts = countmatrix,
+                                    cval = 100, 
+                                    genome = 'hg19', 
+                                    seed = 100, 
+                                    snp_nbhd = 250)
+      
+      #' QC measures
+      qc = facets_fit_qc(fit)
+      qc_fail = names(grep(pattern = 'FALSE', qc, value = T))
+      qc_fail = qc_fail[!qc_fail %in% c('wgd', 'facets_qc')]
+      
+      #' gene-level calls
+      gene_call = facetsSuite::gene_level_changes(facets_output = fit, 
+                                                  genome = 'hg19')
+      
+      gene_call = gene_call[which(gene_call$gene %in% genes_IMPACT), ]
+      gene_call = gene_call[!gene_call$filter %in% c('suppress_segment_too_large'), ]
+      
+      sub_summary = data.frame(sample = basename(files),
+                               qc = qc$facets_qc,
+                               notes = paste(qc_fail, collapse = ';'))
+      
+      sample_summary = rbind(sample_summary, sub_summary)
+      
+      #' merge all objects
+      out = list(facets_fit = fit, 
+                 gene_level = gene_call,
+                 qc = qc)
+      saveRDS(object = out, file = paste0(folder, '/FacetsFits/', basename(files), '_fitted.rds'))
+      rm(countmatrix, fit, qc, qc_fail, gene_call, out)
+    }
+    all_out = rbind(all_out, sample_summary)
+  })
+} 
+
+write.table(all_out, file = 'SampleSummary_QC.txt', sep = '\t', quote = F, row.names = F)
+
